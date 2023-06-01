7b50fc756fdc19908c557bce9464f70214659a70
Giulio <giulioguzatti@hotmail.com>
2023-06-01 00:21:08 -0300

    Adicionando src

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,13 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <div className="rodape">
+            <footer>
+                <img src="/imagens/fundo.png" alt='Rodapé da página da Organo'/>
+            </footer>
+        </div>
+    )
+}
+
+export default Rodape
